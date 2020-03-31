66306375a41d2f0f727eed1e98dc5d6f62519380
0And1Story <witherman233@gmail.com>
2020-03-31 16:51:49 +0800

    Improve feature 'Search'

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,6 @@
 var file_list = [];
 var search_result = [];
+var display_result = [];
 
 function initSearch() {
     fetch('/api/file-list')
@@ -7,31 +8,31 @@ function initSearch() {
     .then(data => file_list = data.list)
     .then(function() {
         search_result = file_list;
-        if (search_result.length > 100) search_result = search_result.slice(0, 100);
-        updateSearchList();
+        initSearchList();
     });
 }
 
-function updateSearchList() {
+function initSearchList() {
     let list = document.getElementById('search-list');
-    list.innerHTML = '';
+    let content = '';
     for (result of search_result) {
-        list.innerHTML += 
+        content += 
         `<li class="mdui-list-item mdui-ripple mdui-text-color-${result.isFile ? filetype.getFileColor(result.file) : 'indigo-accent folder'}">
-            <a href="javascript:;" class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-${result.isFile ? filetype.getIconColor(result.file) : 'indigo-accent folder'} mdui-list-item-avator">${result.isFile ? filetype.getFileIcon(result.file) : 'folder'}</a>
-            <a href="javascript:;" class="mdui-list-item-content mdui-text-color-${result.isFile ? filetype.getFileColor(result.file) : 'indigo-accent folder'}">
+            <a href="${result.path}" target="_blank" class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-${result.isFile ? filetype.getIconColor(result.file) : 'indigo-accent folder'} mdui-list-item-avator">${result.isFile ? filetype.getFileIcon(result.file) : 'folder'}</a>
+            <a href="${result.path}" target="_blank" class="mdui-list-item-content mdui-text-color-${result.isFile ? filetype.getFileColor(result.file) : 'indigo-accent folder'}">
                 <div class="mdui-list-item-title">${result.file}</div>
                 <div class="mdui-list-item-text mdui-list-item-one-line">~${result.path}</div>
             </a>
-            ${result.isFile ? '<a class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-theme-accent" href="javascript;" download="' + result.file + '">file_download</a>' : ''}
+            ${result.isFile ? '<a class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-theme-accent" href="' + result.path + '" download="' + result.file + '">file_download</a>' : ''}
+            <a class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-theme-accent" href="${result.path}/.." target="_blank">folder</a>
         </li>
         <li class="mdui-divider-inset mdui-m-y-0"></li>`;
     }
+    list.innerHTML = content;
 }
 
 function search() {
     let str = document.getElementById('search-input').value.toLowerCase();
     search_result = file_list.filter(res => res.file.toLowerCase().indexOf(str) != -1);
-    if (search_result.length > 100) search_result = search_result.slice(0, 100);
-    updateSearchList();
+    initSearchList();
 }
